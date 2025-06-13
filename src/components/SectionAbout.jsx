b118163a78a74d8cee642e2eb50c4e889b946c5f
nombreusuario <correo@usuario.com>
2025-06-12 20:38:44 -0500

    feat: enhance SectionAbout with new content and styling

--- src/components/SectionAbout.jsx
+++ src/components/SectionAbout.jsx
@@ -1,10 +1,15 @@
 import React, { forwardRef } from 'react'
 import '../stylesheets/SectionAbout.css';
+import Button from './Button';
 
 const SectionAbout = forwardRef((props, ref) => {
   return (
     <div id='about' className='section-about' ref={ref}>
-      
+      <h1>About me</h1>
+      <p>Colombian software developer. I'm passionate about creating cool interfaces,
+        mobile apps and websites that look and work great. I'm a fast learner, I work well
+        in a team and I always bet on projects that break the mold.</p>
+      <button className='btn-download'>Download CV</button>
     </div>
   );
 });
